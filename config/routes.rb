58f9aa9d23544f08d0d38b4d2085b0ee095f7190
ccslakey <cromwellslakey@gmail.com>
2016-08-02 20:22:21 -0700

    some sass and a new landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :uploads, only: [:index, :new, :create, :destroy]
-  root 'uploads#index'
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
